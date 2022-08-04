3ddfbea596d7ab859af713fc8e84df6a58836712
Gaikawade <gmhs6199@gmail.com>
2022-08-04 11:26:47 +0530

    dotenv configure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -9,13 +10,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(multer().any());
 
-let DBString = "mongodb+srv://Mahesh8985:lz9fOW52615YVat4@cluster0.l5fafvk.mongodb.net/productsManagement";
+let DBString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.l5fafvk.mongodb.net/${process.env.DB_NAME}`;
 mongoose.connect(DBString, {useNewUrlParser: true})
-        .then(() => console.log('MongoDB is connected'))
+        .then(() => console.log(`Hey man...I'm ready to store yout Data`))
         .catch((err) => console.log(err));
 
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, () =>{
-    console.log(`Express Server is running on port ${process.env.PORT || 3000}`);
+    console.log(`I'm Express and I'm serving you on port ${process.env.PORT || 3000}`);
 });
